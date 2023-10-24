89e8ae6ae13ec3f52a3a3dbd214678534c4e84dc
Diegojacober <diegoalencar.jacober@gmail.com>
2023-10-24 17:02:31 -0300

    iniciando validacao

--- src/main/java/com/diegojacober/authservice/auth/RegisterRequest.java
+++ src/main/java/com/diegojacober/authservice/auth/RegisterRequest.java
@@ -2,6 +2,8 @@ package com.diegojacober.authservice.auth;
 
 import com.diegojacober.authservice.user.Role;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +15,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+    @NotBlank(message = "firstname is required.")
     private String firstname;
+
+    @NotBlank(message = "lastname is required.")
     private String lastname;
+
+    @NotBlank(message = "email is required.")
+    @Email
     private String email;
+
+    @NotBlank(message = "password is required.")
     private String password;
+
     private Role role;
 }
